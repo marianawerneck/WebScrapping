a94b906faf5db0acca9fecba9758e91180af13c1
marianawerneck <33205506+marianawerneck@users.noreply.github.com>
2018-03-06 17:34:20 -0300

    pegando varios ids

--- avonPageWebScrapping.py
+++ avonPageWebScrapping.py
@@ -5,17 +5,32 @@ import json
 #urllib2
 import requests as urllib2
 #specify the url
-avon = "https://www.avonstore.com.br/api/catalog_system/pub/products/search/?fq=productId:20441"
+avon = "https://www.avonstore.com.br/api/catalog_system/pub/products/search/?fq=productId:"
 #query the website and return the html to the variable 'page'
-page = urllib2.get(avon)
+#page = urllib2.get(avon)
 from bs4 import BeautifulSoup
 
 #print(json.dumps(page.text, sort_keys=True, indent=4));
 
-data = (page.text).split(',')
-for i in data:
-    if '"Composição":[' in i:
-        copia = i[15:-3]
-copia = copia.split('; ')
-for i in copia:
-    print(i)
+print("--------------------")
+
+def printComposition(jsonText):
+    copia = ''
+    data = (page.text).split(',')
+    for i in data:
+        if '"Composição":[' in i:
+            copia = i[15:-3]
+    copia = copia.split('; ')
+    for i in copia:
+        print(i)
+    print("--------------------")
+
+for i in range(20440,20450):
+    actualPage = avon + str(i)
+    page = urllib2.get(actualPage)
+    printComposition(page.text)
+
+
+
+
+
